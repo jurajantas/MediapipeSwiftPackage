6a16b4c2dd6e40fade8c194328c5b5cf117582fe
Juraj Antas <juraj.antas@gmail.com>
2026-05-19 23:41:07 +0200

    Try to make package dynamic

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftTasksVision",
+            type: .dynamic,
             targets: ["SwiftTasksVision"]
         ),
         .executable(
