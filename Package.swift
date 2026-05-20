fabf6b735a3f2b6bb99fba49778b81fbcca33e5f
Juraj Antas <juraj.antas@gmail.com>
2026-05-20 22:55:32 +0200

    Removed .unsafeFlags(["-ObjC"]),

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
             name: "SwiftTasksVision",
             dependencies: ["MediaPipeCommonGraphLibraries", "MediaPipeTasksVision", "MediaPipeTasksCommon"],
             linkerSettings: [
-                .unsafeFlags(["-ObjC"]),
+                //.unsafeFlags(["-ObjC"]),
                 .linkedFramework("UIKit"),
                 .linkedFramework("Metal"),
                 .linkedFramework("MetalKit"),
